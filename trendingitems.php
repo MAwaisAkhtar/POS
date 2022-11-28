dfe7e9cbccb07e91d6abe74ef9d1870f6d6963a8
MAwaisAkhtar <awaismufti90@gmail.com>
2022-11-28 13:20:34 +0500

    s

--- trendingitems.php
+++ trendingitems.php
@@ -1,25 +1,41 @@
 <?php
 require('db.php');
-session_start();
 $res="SELECT `PRODUCT_NAME`, `QUANTITY` FROM `billing_data` ";
 $sel=mysqli_query($con,$res);
-$a_p='';
-$a_q='';
+
+$sel1=mysqli_query($con,$res);
+
+// $a_p='';
+
+// $a_q='';
+
 while ($row=mysqli_fetch_assoc($sel)) {
-  $p=$row['PRODUCT_NAME'];
-  $q=$row['QUANTITY'];
-  if ($p==$a_p) { 
-    // $a_p=$p;
-?>
-<td> <?php echo $q+$a_q; ?> </td>
-<?php
-   $a_p=$p;
-   $a_q=$q;
-  }elseif($p !== $a_p) {
-    $a_p=$p;
-    $a_q=$q;
-  }
-  // echo $p;
-  // echo $q;
+  $data['cart'][]=$row;
 }
-?>
+  foreach ($data['cart'] as $key => $value) {
+   
+
+  $p=$value['PRODUCT_NAME'];
+  
+  $b=0;
+
+  foreach ($data['cart'] as $key => $value){
+
+    $a_p=$value['PRODUCT_NAME'];
+
+    if ($p==$a_p) { 
+
+      $a_q=$value['QUANTITY'];
+
+       $b=$a_q+$b;
+
+       }
+       
+      }
+  }
+      ?>
+
+      <td><?php echo $p ?></td>
+
+      <td><?php echo $b ?></td>
+      
